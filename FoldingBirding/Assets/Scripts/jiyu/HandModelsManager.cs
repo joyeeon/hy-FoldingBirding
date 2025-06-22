62267032428e328ba0aad48d69ec0c30335dad0e
JiyuKim04 <rlawldb4396@swu.ac.kr>
2025-06-22 15:58:40 +0900

    0622

--- FoldingBirding/Assets/Scripts/jiyu/HandModelsManager.cs
+++ FoldingBirding/Assets/Scripts/jiyu/HandModelsManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class MotionDisplayer : MonoBehaviour
+public class HandModelsManager : MonoBehaviour
 {
     [Tooltip("이 버튼이 선택되었을 때 보여줄 Motion UI 오브젝트")]
     public GameObject motionUI;
@@ -8,14 +8,14 @@ public class MotionDisplayer : MonoBehaviour
     [Tooltip("다른 MotionDisplayer들이 보여준 UI를 숨기려면 true")]
     public bool hideOthersOnShow = true;
 
-    private static MotionDisplayer[] allDisplayers;
+    private static HandModelsManager[] allDisplayers;
 
     private void Awake()
     {
         // 처음 한 번만 전체 목록 확보
         if (allDisplayers == null)
         {
-            allDisplayers = FindObjectsOfType<MotionDisplayer>();
+            allDisplayers = FindObjectsOfType<HandModelsManager>();
         }
 
         // 시작 시에는 꺼두기 (원하면 제거 가능)
